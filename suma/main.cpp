8544da6bf968f3e9d391e2cb2a686c70b78d62c3
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-16 09:41:17 -0500

    lo que quieras

---
+++ suma/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout << "ingresa 2 numeros : " << endl;
+    cin >> a>>b;
+    cout << "a+b : " << a+b;
+
+    return 0;
+}
